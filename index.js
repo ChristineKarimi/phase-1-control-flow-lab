c74952add4d6d5916ab2cf4e985f308115a2a5ee
christinekarimi <karimikim3@gmail.com>
2022-02-07 15:28:29 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+// Write your code in this file!
+function scuberGreetingForFeet(feet){
+  return (feet > 2500)? 'No can do.' : (feet <= 400)? 'This one is on me!' :'I will gladly take your thirty bucks.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return (city === 'NYC')? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    case 'thanks for everything':
+      return 'Bye.';
+  }
 }
